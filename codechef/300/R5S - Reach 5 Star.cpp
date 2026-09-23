a8d55bbcf783172e5bde1435d7fb271c51d58c7a
Akhilreddy-GIT <kns.akhilreddy@gmail.com>
2026-09-23 20:23:41 +0530

    Add Reach 5 Star (300)

---
+++ codechef/300/R5S - Reach 5 Star.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    if (X + Y >= 2000)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
